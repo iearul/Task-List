a2c9432f9a6c9825741ecd68765679662b1a5475
Md Iearulialam <iearul.abid@gmail.com>
2021-02-20 01:37:19 +0100

    my 1st commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+//Define UI element
+let form = document.querySelector('#task_form');
+let taskList = document.querySelector('ul');
+let clrBtn = document.querySelector('#clear_task_btn');
+let filter = document.querySelector('#task_filter');
+let taskInput = document.querySelector('#add_task');
+
+//Define event listner 
+form.addEventListener('submit', addTask);
+taskList.addEventListener('click', removeTask);
+clrBtn.addEventListener('click', clearTask);
+
+//Define Funtion
+function addTask(e){
+    if(taskInput.value==='')
+    {
+        alert('Add a task');
+    }
+    else
+    {
+        //Create li element
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(taskInput.value + " "));
+        let link = document.createElement('a');
+        link.setAttribute('href', '#');
+        link.innerHTML='X';
+        li.appendChild(link);
+        taskList.appendChild(li);
+        taskInput.value='';
+
+    }
+    e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e)
+{
+    if(e.target.hasAttribute('href'))
+    {
+        if(confirm('Are you sure?') )
+        {
+            e.target.parentElement.remove();
+        }
+    }
+}
+
+//Clear Task
+function clearTask(e)
+{
+    if(confirm('Are you confirm to delete all task'))
+    {
+        taskList.innerHTML='';
+    }
+}
